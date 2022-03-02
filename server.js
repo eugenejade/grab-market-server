c3390e757b419b5cc5428a14db9476c018ada98b
eugene_jade <eugene_jade@dkitec.com>
2022-03-02 18:11:40 +0900

    banner 추가

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const app = express();
 const port = 8080;
 const models = require("./models");
 const multer = require("multer");
-const uploads = multer({
+const { error } = require("console");
+const upload = multer({
   storage: multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, "uploads/");
@@ -17,6 +18,44 @@ const uploads = multer({
 app.use(express.json());
 app.use(cors());
 app.use("/uploads", express.static("uploads"));
+
+app.get("/banners", (req, res) => {
+  models.Banner.findAll({
+    limit: 2,
+  })
+    .then((result) => {
+      res.send({
+        banners: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("에러가 발생했습니다");
+    });
+});
+
+app.post("/purchase/:id", (req, res) => {
+  const { id } = req.params;
+  models.Product.update(
+    {
+      soldout: 1,
+    },
+    {
+      where: {
+        id,
+      },
+    }
+  )
+    .then((result) => {
+      res.send({
+        result: true,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("에러가 발생했습니다.");
+    });
+});
 
 app.get("/products", (req, res) => {
   models.Product.findAll({
@@ -36,9 +75,10 @@ app.get("/products", (req, res) => {
     });
 });
 
-app.post("/image", uploads.single("image"), (req, res) => {
-  const file = req.file;
+app.post("/image", upload.single("image"), (req, res) => {
+  const file = req.file; //저장된 이미지 정보 get
   console.log("file : ", file);
+  console.log("file path====", file.path);
   res.send({
     imageUrl: file.path,
   });
@@ -46,7 +86,7 @@ app.post("/image", uploads.single("image"), (req, res) => {
 
 app.post("/products", (req, res) => {
   const body = req.body;
-  const { name, description, price, seller } = body;
+  const { name, description, price, seller, imageUrl } = body;
   if (!name || !description || !price || !seller) {
     res.send("모든 필드를 입력해주세요.");
   }
